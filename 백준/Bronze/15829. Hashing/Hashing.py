5a7dc22695fbaa3471841f018f0c0f70165dc8ab
MinuLe0382 <165364653+MinuLe0382@users.noreply.github.com>
2024-08-05 02:50:22 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,6 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+table = {'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5, 'f' : 6, 'g' : 7,
+        'h' : 8, 'i' : 9, 'j' : 10, 'k' : 11, 'l' : 12, 'm' : 13, 'n' : 14, 'o' : 15,
+        'p' : 16, 'q' : 17, 'r' : 18, 's' : 19, 't' : 20, 'u' : 21, 'v' : 22, 'w' : 23,
+        'x' : 24, 'y' : 25, 'z' : 26}
+
 hash = 0
 l = int(input())
 string = input()
 for i in range(l):
-    hash += int(ord(string[i]) - ord('a') + 1) * (31 ** i)
-print(hash)
+    hash += table[string[i]] * (31 ** i)
+print(hash % 1234567891)
