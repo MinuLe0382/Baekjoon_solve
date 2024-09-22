3a3511d8aff7fb6f2d8d76c35c7798b580c95531
MinuLe0382 <165364653+MinuLe0382@users.noreply.github.com>
2024-09-22 23:09:27 +0900

    [Silver II] Title: 숫자판 점프, Time: 40 ms, Memory: 31632 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
@@ -1,22 +1,31 @@
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
-#DFS
-def DFS(r, c, num):
-    #방문 함수에 들어오는거 자체가 방문
-    if len(num) == 6:
-        cases.add(num)
+board = [list(map(int, input().split())) for _ in range(5)]
+
+# 중복을 피하기 위해 set 사용
+result_set = set()
+
+# 상하좌우 이동을 위한 방향 벡터
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+# DFS 함수 정의
+def dfs(x, y, number):
+    # 6자리 숫자가 되면 결과에 추가
+    if len(number) == 6:
+        result_set.add(number)
         return
-    # 탐색
-    for d in range(4):
-        nr, nc = r + dr[d], c + dc[d]
-        if 0 <= nr < 5 and 0 <= nc < 5:
-            DFS(nr, nc, num + board[nr][nc])
+    
+    # 상하좌우로 이동하며 탐색
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        # 좌표가 유효한 범위 내에 있을 때만 이동
+        if 0 <= nx < 5 and 0 <= ny < 5:
+            dfs(nx, ny, number + str(board[nx][ny]))
 
-board = [input().split() for _ in range(5)]
-cases = set()
+# 모든 좌표에서 DFS 탐색 시작
+for i in range(5):
+    for j in range(5):
+        dfs(i, j, str(board[i][j]))
 
-for r in range(5):
-    for c in range(5):
-        DFS(r, c, board[r][c])
-        
-print(len(cases))
+# 결과 출력
+print(len(result_set))
