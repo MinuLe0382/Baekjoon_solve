e9bce37221799c42cd691ca00de1cc981a4c8302
MinuLe0382 <165364653+MinuLe0382@users.noreply.github.com>
2024-11-18 20:10:50 +0900

    [Gold IV] Title: 타임머신, Time: 1160 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.py
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -1,29 +1,27 @@
-N, M = map(int, input().split())
+import sys
+input = sys.stdin.readline
 
-edges = []
-for _ in range(M):
-    s, e, w = map(int, input().split())
-    edges.append((s, e, w))
-    
-dist = [float('INF')] * (N + 1)
+INF = 987654321
+
+n, m = map(int, input().rstrip().split())
+dist = [INF] * (n + 1)
+
+alist = [list(map(int, input().rstrip().split())) for _ in range(m)]
+
 dist[1] = 0
-
-for _ in range(N):
-    updated = False
-    for cur, nxt, w in edges:
-        if dist[cur] == float('INF'): # 사용불가한 간선
-            continue
-        if dist[nxt] > dist[cur] + w:
-            dist[nxt] = dist[cur] + w # 업데이트
-            updated = True
-    if updated == False: # 이게 최선의 배열이라는 의미
-        break
-# 위 for문의 결과는 다익스트라의 결과와 완전히 동일하다.
-if updated == True:
+flag = False
+for i in range(n):
+    for start, end, weight in alist:
+        if dist[start] != INF and dist[end] > dist[start] + weight:
+            dist[end] = dist[start] + weight
+            if i == n - 1:
+                flag = True
+                
+if flag:
     print(-1)
 else:
-    for i in range(2, N + 1):
-        if dist[i] == float('INF'):
-            print(-1)
-        else:
+    for i in range(2, n + 1):
+        if dist[i] != INF: 
             print(dist[i])
+        else:
+            print(-1)
