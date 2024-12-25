9370964d33b072b0095eedf00b0af00798f4c46a
MinuLe0382 <165364653+MinuLe0382@users.noreply.github.com>
2024-12-25 22:34:15 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int N = -1;
+    std::cin >> N;
+    if (N == 0)
+        std::cout << "YONSEI";
+    else
+        std::cout << "Leading the Way to the Future";
+    return 0;
+}
