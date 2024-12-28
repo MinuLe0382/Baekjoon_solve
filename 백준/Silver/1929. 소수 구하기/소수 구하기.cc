dbf8db8ce3e20020b436514d4f31b8df5ff9d017
MinuLe0382 <165364653+MinuLe0382@users.noreply.github.com>
2024-12-29 00:19:01 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,29 +1,38 @@
 #include <iostream>
-#include <cmath>
-#include <vector>
+using namespace std;
+
+#define MAX 1000000
+
+int arr[MAX + 1]{ 0, };
 
 int main()
 {
-    int N, M = 0;
-    std::cin >> M >> N;
-    std::vector<int> checker(N + 1, 0);
-    for (int k = 0; k < N + 1; k++)
-        checker[k] = k;
-    checker[1] = 0;
-    for (int i = 2; i <= int(pow(N, 0.5)); i++)
+    int m, n;
+    cin >> m >> n;
+
+    for (int i = 2; i <= n; i++)
     {
-        if (checker[i] != 0)
+        arr[i] = i;
+    }
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (arr[i] == 0)
         {
-            for (int j = i * i; j < N + 1; j += i)
-                checker[j] = 0;
+            continue;
+        }
+        for (int j = i * i; j <= n; j += i)
+        {
+            arr[j] = 0;
         }
     }
 
-    for (int i = M; i < N + 1; i++)
+    for (int i = m; i <= n; i++)
     {
-        if (checker[i] != 0)
-            std::cout << checker[i] << std::endl;
+        if (arr[i] != 0)
+        {
+            cout << arr[i] << '\n';
+        }
     }
-
     return 0;
 }
